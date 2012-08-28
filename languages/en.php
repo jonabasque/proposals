baa251b86eadc819578863507d1a0486184938f6
Pablo Martin <caedes@sindominio.net>
2012-08-28 18:43:18 +0000

    initial commit.

---
+++ languages/en.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Assembly English language file.
+ *
+ */
+
+$english = array(
+	'proposals' => 'Proposals',
+	'proposals:enableproposals' => 'Enable group proposals',
+	'proposals:proposal:group' => 'Group decissions',
+
+	// Proposals
+	'proposals:proposal:none' => 'No proposals created yet',
+	'item:object:proposal' => 'Proposals',
+	'proposal:add' => 'Create proposal',
+
+	// Proposal attributes
+	'proposals:proposal:title' => 'Title',
+	'proposals:proposal:description' => 'Description',
+	'proposals:proposal:tags' => 'Tags',
+	'proposals:proposal:access_id' => 'Access',
+	'proposals:proposal:owner' => 'Created by %s',
+
+	// Decissions
+	'proposals:decission:none' => 'No decissions created yet',
+	'item:object:decission' => 'Decissions',
+	'decission:add' => 'Create decission',
+	'proposals:decission:children' => 'Proposals',
+	'crud:decission:nochildren' => 'No proposals created yet',
+
+	// Decission attributes
+	'proposals:decission:title' => 'Title',
+	'proposals:decission:description' => 'Description',
+	'proposals:decission:tags' => 'Tags',
+	'proposals:decission:access_id' => 'Access',
+	'proposals:decission:owner' => 'Created by %s',
+
+	// Group module
+	'proposals:proposal:group' => 'Group proposals',
+	'proposals:proposal:write' => 'Write a proposal',
+
+	// river
+	'river:create:object:decission' => '%s submitted a decission %s',
+	'river:create:object:proposal' => '%s submitted a proposal %s',
+	'river:edited:object:decission' => '%s edited a decission %s',
+	'river:edited:object:proposal' => '%s edited a proposal %s',
+	'river:comment:object:decission' => '%s commented on the decission %s',
+	'river:comment:object:proposal' => '%s commented on the proposal %s',
+
+
+);
+
+add_translation('en', $english);
---
+++ views/default/object/decission.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * View for decission object
+ *
+ * @package Proposals
+ *
+ * @uses $vars['entity']    The decission object
+ * @uses $vars['full_view'] Whether to display the full view
+ */
+
+echo elgg_view('crud/object', $vars);
