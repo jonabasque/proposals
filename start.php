8ac7a1c3c9236f01ab46c078048803a8da6fd07b
Sem <sembrestels@riseup.net>
2012-08-31 03:11:25 +0200

    Moved owner block logic to crud.

--- start.php
+++ start.php
@@ -20,12 +20,6 @@ function proposals_init() {
 	// add to the main css
 	elgg_extend_view('css/elgg', 'proposals/css');
 
-	// add group assemblies link to
-	elgg_register_plugin_hook_handler('register', 'menu:owner_block', 'assemblies_owner_block_menu');
-
-	// add group assemblies link to
-	elgg_register_plugin_hook_handler('register', 'menu:owner_block', 'proposals_owner_block_menu');
-
 	// Add group option
 	add_group_tool_option('proposals', elgg_echo('proposals:enableproposals'), false);
 	elgg_extend_view('groups/tool_latest', 'proposals/group_module');
@@ -66,19 +60,4 @@ function proposals_init() {
 	$crud = crud_register_type('proposal', $variables);
 	#$crud->children_type = 'agenda_point';
 	$crud->module = 'proposals';
-}
-
-/**
- * Add a menu item to an ownerblock
- */
-function proposals_owner_block_menu($hook, $type, $return, $params) {
-	if (elgg_instanceof($params['entity'], 'group')) {
-		if ($params['entity']->proposals_enable == "yes") {
-			$url = "decission/owner/{$params['entity']->guid}";
-			$item = new ElggMenuItem('proposals', elgg_echo('proposals:proposal:group'), $url);
-			$return[] = $item;
-		}
-	}
-
-	return $return;
 }
