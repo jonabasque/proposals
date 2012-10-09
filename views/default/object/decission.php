8779c4c37e669991f83a8d1901aea24e26ccbafa
Sem <sembrestels@riseup.net>
2012-10-09 20:08:05 +0200

    Added subtitle to decision object.

--- views/default/object/decission.php
+++ views/default/object/decission.php
@@ -8,4 +8,9 @@
  * @uses $vars['full_view'] Whether to display the full view
  */
 
+if ($vars['full_view']) {
+	$title = elgg_echo('proposals:decision');
+	echo "<h3>$title</h3>";
+}
+
 echo elgg_view('crud/object', $vars);
